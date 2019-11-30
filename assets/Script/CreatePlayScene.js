663d17b7b8fe2f8dfc25aa4753db755eaf7440e1
Rythenov <376973015@qq.com>
2019-11-30 19:54:46 +0800

    添加创建角色部分脚本

--- assets/Script/CreatePlayScene.js
+++ assets/Script/CreatePlayScene.js
@@ -16,16 +16,24 @@ cc.Class({
         m_playerInfo : cc.Sprite,
         m_leftButton : cc.Button,
         m_rightButton : cc.Button,
+        m_allPlayerPicsVec: {
+            type: cc.Texture2D,
+            default: []
+        },
+        m_text : cc.TextAsset,
         m_playerPicBeginPos : cc.v2(-1300, 0),
         m_playerPicEndPos : cc.v2(-500, 0),
         m_playerInfoBeginPos : cc.v2(1400, 260),
         m_playerInfoEndPos : cc.v2(480, 260),
-        m_loadTime : 1
+        m_loadTime : 1,
+        m_currentPlayerPicIndex : 0,
     },
 
     // LIFE-CYCLE CALLBACKS:
 
     onLoad () {
+        //初始化左侧立绘
+        this.m_playerPic.spriteFrame.setTexture(this.m_allPlayerPicsVec[this.m_currentPlayerPicIndex]);
         //左侧立绘动画
         this.m_playerPic.node.setPosition(this.m_playerPicBeginPos);
         var playerPicMoveAct = cc.moveTo(this.m_loadTime / 2, this.m_playerPicEndPos);
@@ -37,11 +45,59 @@ cc.Class({
     },
 
     onLeftButtonClicked : function() {
-
+        //获取资源序号
+        var toUseIndex = this.getLeftPlayerPicIndex(this.m_currentPlayerPicIndex); 
+        this.m_currentPlayerPicIndex = toUseIndex;  
+        this.changePicAction();
     },
 
     onRightButtonClicked : function() {
+        //获取资源序号
+        var toUseIndex = this.getRightPlayerPicIndex(this.m_currentPlayerPicIndex);
+        this.m_currentPlayerPicIndex = toUseIndex;
+        this.changePicAction();
+    },
 
+    changePicAction : function(){   
+        //收起立绘动作
+       var closePlayerMoveAct = cc.moveTo(this.m_loadTime / 2, this.m_playerPicBeginPos);
+       //推出立绘动作
+       var openPlayerMoveAct = cc.moveTo(this.m_loadTime / 2, this.m_playerPicEndPos);
+       //序列
+       var changPlayerSequence = cc.sequence(cc.callFunc(this.leftRightButtonEnable, this, false)
+           , closePlayerMoveAct
+           , cc.callFunc(this.changePic, this)
+           , openPlayerMoveAct
+           , cc.callFunc(this.leftRightButtonEnable, this, true));
+       this.m_playerPic.node.runAction(changPlayerSequence);
+    },
+
+    getLeftPlayerPicIndex : function(currentIndex) {
+        var vecSzie = this.m_allPlayerPicsVec.length;
+        if(currentIndex == 0)
+        {
+            return vecSzie - 1;
+        }
+        return currentIndex - 1;
+    },
+
+    getRightPlayerPicIndex : function(currentIndex) {
+        var vecSzie = this.m_allPlayerPicsVec.length;
+        if(currentIndex == vecSzie - 1)
+        {
+            return 0;
+        }
+        return currentIndex + 1;
+    },
+
+    changePic : function(){
+        this.m_playerPic.spriteFrame = new cc.SpriteFrame(this.m_allPlayerPicsVec[this.m_currentPlayerPicIndex]);
+    },
+
+    leftRightButtonEnable : function(target, isEnable){
+        //回调第一个必须接target
+        this.m_rightButton.interactable = isEnable;
+        this.m_leftButton.interactable = isEnable;
     },
 
     start () {
